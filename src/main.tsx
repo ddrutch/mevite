1c6bcd857058b923e035ba5b6334207e6e8586bc
Brandon K <futurologizt@gmail.com>
2025-01-09 10:36:07 +0200

    we added tings

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,12 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
-
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
